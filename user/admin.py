079b10f0530ebb942f3342d372a8a45837b8456a
Your Name <you@example.com>
2023-06-13 16:49:50 +0330

    add Account class to user/models.py

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'first_name', 'last_name', 'acctive', 'image')
+    list_filter = ('acctive',)
+    search_fields = ('user_name', 'first_name', 'last_name')
+
+    fieldsets = (
+        (None, {'fields': ('user_name', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'bio', 'image')}),
+        ('Permissions', {'fields': ('acctive',)}),
+    )
--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Account(models.Model):
+    image = models.ImageField(
+        _("Image"), upload_to='profile_images', null=True, blank=True) 
+    bio = models.CharField(_("Bio"), max_length=150) 
+    user_name = models.CharField(_("User Name"), max_length=50)
+    password = models.CharField(_("Password"), max_length=50)    
+    first_name = models.CharField(_("First Name"), max_length=50)
+    last_name = models.CharField(_("Last Name"), max_length=50)
+    acctive = models.BooleanField(_("Is Active Account?"), default=False) 
+
+
+    class Meta:
+        verbose_name = _("Account")
+        verbose_name_plural = _("Account")
+
+
+    def __str__(self) -> str:
+        return self.user_name    
