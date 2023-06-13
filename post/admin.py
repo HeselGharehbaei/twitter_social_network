23745871a85b93aed8efda737df12c1322acc375
Your Name <you@example.com>
2023-06-13 17:24:40 +0330

    add Post model to post/models.py

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Post, Tag, Comment, Image
 
-# Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class TagInline(admin.TabularInline):
+    model = Tag
+    fk_name = 'post'
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')  
+    list_filter = ('user', 'acctive')
+    search_fields = ('title', 'tag')
+    inlines = [ImageInline, CommentInline, TagInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image',)   
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'text',) 
+    list_display = ('title', 'post', 'created_at')
+    list_filter = ('post',)
+     
--- post/models.py
+++ post/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey("user.Account",
+                                verbose_name=_("Account"),
+                                on_delete=models.CASCADE
+                                )
+    text = models.TextField(_("Post")) 
+    title = models.CharField(_("Title"), max_length=100) 
+    acctive = models.BooleanField(_("Is Post Archive?"), default=False)      
+    create_at = models.DateTimeField(
+        _("create at"), auto_now=False, auto_now_add=True)
+    tag = models.ManyToManyField(_("Tag"),
+                                    verbose_name=_("Tags"),
+                                    )  
+
+
+    class Meta:
+        verbose_name = _("Post")
+        verbose_name_plural = _("Post")
+
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(_("Name"), max_length=50)
+
+
+class Image(models.Model):
+    name = models.CharField(_("Name"), max_length=50)
+    alt = models.CharField(_("Alternative Text"), max_length=100)
+    Post = models.ForeignKey("Post",
+                                verbose_name=_("Post"),
+                                on_delete=models.CASCADE)
+    image = models.ImageField(_("Image"), upload_to='Post',)
+
+
+class Comment(models.Model):
+    title = models.CharField(_("Title"), max_length=150)
+    text = models.TextField(_("Text"))
+    parent = models.ForeignKey("self",
+                               verbose_name=_("Parent Comment"),
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True
+                               )
+    user = models.ForeignKey("user.Account",
+                        verbose_name=_("Account"),
+                        on_delete=models.CASCADE
+                        )   
+    Post = models.ForeignKey("Post",
+                            verbose_name=_("Post"),
+                            on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comment")
+
+
+    def __str__(self) -> str:
+        return self.title
