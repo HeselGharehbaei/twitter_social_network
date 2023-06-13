791e3ad68a0f1793f403987d84e6e39400f27032
Your Name <you@example.com>
2023-06-13 18:01:35 +0330

    add Like class to core/models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Like(models.Model):
+    like = models.BooleanField(_("Is Like?"), default=False) 
+    Post = models.ForeignKey("post.Post",
+                                verbose_name=_("Post"),
+                                on_delete=models.CASCADE)
