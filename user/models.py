69dd8358866ace7892f5974abcdf6b77b18ff68d
Your Name <you@example.com>
2023-06-18 14:40:43 +0330

    delete Account class and class User(AbstractUser) added to user/models.py

--- user/models.py
+++ user/models.py
@@ -1,25 +1,35 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.contrib.auth.models import AbstractUser
 
 
-class Account(models.Model):
-    image = models.ImageField(
-        _("Image"), upload_to='profile_images', null=True, blank=True) 
-    bio = models.CharField(_("Bio"), max_length=150) 
-    user_name = models.CharField(_("User Name"), max_length=50)
-    password = models.CharField(_("Password"), max_length=50)    
-    first_name = models.CharField(_("First Name"), max_length=50)
-    last_name = models.CharField(_("Last Name"), max_length=50)
-    acctive = models.BooleanField(_("Is Active Account?"), default=False) 
+class User(AbstractUser):
+    email = models.EmailField(
+        _('email address'),
+        unique=True
+    )
+    bio = models.CharField(
+        _("Biography"), 
+        max_length=150, 
+        help_text=_("Biography is a written account of a person's life"), 
+        blank= True, 
+        null= True,
+    )
+    image = models.FileField(
+        _("Image"), 
+        upload_to="uploads/photos"
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
     class Meta:
-        verbose_name = _("Account")
-        verbose_name_plural = _("Account")
+    verbose_name = _("Account")
+    verbose_name_plural = _("Account")
 
 
-    def __str__(self) -> str:
-        return self.user_name   
+    def __str__(self):
+    return self.email     
 
 
 class Follow(models.Model):
