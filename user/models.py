66609f70965cbfd98bd5e96845e507fd0395ddf4
hesel <hgharehbaai@gmail.com>
2023-06-19 14:43:28 +0330

    delete user model and add account model to user/models.py

--- user/models.py
+++ user/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-from django.utils.translation import gettext as _
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
+class Account(AbstractBaseUser):
+    phone_number= models.CharField(
+        _('Phone Number'),
+        max_length=11, unique=True, 
+        validators=[
+            RegexValidator(regex="\A(09)(0|1|2|3)[0-9]{7}\d\Z", message="phone number is not correct")
+        ])
+    username = models.CharField(
+        _('User Name'),
+        max_length=50, unique=True)
     email = models.EmailField(
         _('email address'),
+        max_length= 254,
         unique=True
     )
     bio = models.CharField(
@@ -17,24 +28,34 @@ class User(AbstractUser):
     )
     image = models.FileField(
         _("Image"), 
-        upload_to="uploads/photos"
+        upload_to="uploads/photos", blank= True, null= True
     )
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-
-    class Meta:
-    verbose_name = _("Account")
-    verbose_name_plural = _("Account")
-
-
-    def __str__(self):
-    return self.email     
-
-
-class Follow(models.Model):
-    from_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='following')
-    to_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='followers')
-    created_at = models.DateField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(
+        _('First Name'),
+        max_length=50, 
+        blank=True, 
+        null=True)
+    last_name = models.CharField(
+        _('Last Name'),
+        max_length=50, 
+        blank=True, 
+        null=True)
+    is_active = models.BooleanField(default=True)
+    image = models.FileField(
+        _("Image"), 
+        upload_to="uploads/photos",
+        blank= True, 
+        null= True
+    )
+    date_of_birth = models.DateField(blank=True, null=True)
     
 
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'email']
+    
+
+    def __str__(self):
+        return self.username
