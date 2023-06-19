461b1d1294873a155c0cb1958d3cb49a134dac27
hesel <hgharehbaai@gmail.com>
2023-06-19 14:50:48 +0330

    makemigrations of user/models.py

--- post/admin.py
+++ post/admin.py
@@ -1,25 +1,33 @@
 from django.contrib import admin
-from .models import Post, Tag, Comment, Image
+from django.utils.translation import gettext as _
+from .models import Post, Tag, Image, Comment, Like
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    classes = ['collapse']
 
-
-class CommentInline(admin.StackedInline):
+class CommentInline(admin.TabularInline):
     model = Comment
+    classes = ['collapse']
 
 
-class TagInline(admin.TabularInline):
-    model = Tag.posts.through
-    extra = 1
+class LikInline(admin.TabularInline):
+    model = Like   
+    classes = ['collapse']
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user')  
-    list_filter = ('user', 'is_archived')
-    search_fields = ('title', 'tags')
-    inlines = [ImageInline, CommentInline, TagInline]
+    inlines = [ImageInline, CommentInline, LikInline]
+    list_display = ('title', 'user', 'created_at', 'is_archived')
+    list_filter = ('is_archived', 'tags')
+    search_fields = ('title', 'text')
+    filter_horizontal = ('tags',)
+    fieldsets = (
+        (_("Post Information"), {
+            'fields': ('user', 'title', 'text', 'tags', 'is_archived')
+        }),
+    )
 
 
 @admin.register(Tag)
@@ -27,14 +35,23 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('image',)   
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'like')
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    search_fields = ('title', 'text',) 
-    list_display = ('title', 'post')
+    list_display = ('title', 'user', 'post', 'parent')
     list_filter = ('post',)
-     
+    search_fields = ('title', 'text')
+    fieldsets = (
+        (_("Comment Information"), {
+            'fields': ('title', 'text', 'parent', 'user', 'post')
+        }),
+    )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'alt', 'Post')
