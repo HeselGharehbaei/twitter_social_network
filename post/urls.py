f03ac23ab362fbb4d6aff181916464240459962d
hesel <hgharehbaai@gmail.com>
2023-06-20 00:10:28 +0330

    post view

--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post, name='post'),
+    path('post/<int:post_id>/', views.post, name= 'post'),
+    path('search/post', views.search_posts, name='search posts'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Post
+from django.db.models import Q
 
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'post.html', {'posts': posts})
+def post(request, post_id):
+    post= Post.objects.get(id= post_id)
+    return render(request, 'post.html', {'post': post})   
+
+
+def search_posts(request):
+    query = request.GET.get('q')
+    posts = Post.objects.filter(Q(title__icontains=query))
+    context = {'posts': posts}
+    return render(request, 'search_post.html', context)
