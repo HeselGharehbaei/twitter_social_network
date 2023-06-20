f88f90316c77a17638b89265f1331ab43e13fd19
hesel <hgharehbaai@gmail.com>
2023-06-20 10:11:41 +0330

    add base and mixin model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,16 @@
 from django.db import models
-from django.utils.translation import gettext as _
+from uuid import uuid4
+from datetime import datetime
 
 
+class BaseModel(models.Model):
+    id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
 
+    
+    class Meta:
+        abstract = True
+
+
+class TimeStampMixin:
+    created_at = models.DateTimeField(auto_now_add=True, default=datetime.now)
+    updated_at = models.DateTimeField(auto_now=True, default=datetime.now)
