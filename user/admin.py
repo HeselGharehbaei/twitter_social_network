29d57e086effbdda781dae6ca0b1d1af51230c96
hesel <hgharehbaai@gmail.com>
2023-06-27 09:51:28 +0330

    add account to user/models.py

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Account
+from django.utils.translation import gettext as _
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_active', 'email', 'date_joined')
+    list_filter = ('is_active',)
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'bio', 'image', 'email', 'phone_number')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff'),
+            'classes': ('collapse',)
+        }),
+        (_("Date Information"), {
+            'fields': ('date_joined',),
+            'classes': ('collapse',)
+        }),
+    )
+ 
