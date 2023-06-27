c58392aa66883a3c0efb6ac799853e07ca0a0d6e
hesel <hgharehbaai@gmail.com>
2023-06-27 11:50:23 +0330

    creat urls and view for twitter project from user app

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('account/<str:account_username>/', views.AccountView.as_view(), name= 'account'),   
+    path('search/account', views.SearchAccountView.as_view(), name='search account'),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Account, Follow
+from post.models import Post
+from django.db.models import Q
+from django.views import View
+
+
+class HomePageView(View):
+    def get(self, request):
+        accounts_detailes = Account.objects.all()
+        posts_detailes = Post.objects.all()
+        return render(request, 'home.html', {'accounts_detailes': accounts_detailes, 'posts_detailes': posts_detailes})
+
+class AccountView(View):
+    def get(self, request, account_username):
+        accounts = Account.objects.get(username=account_username)
+        following_count, following, followers_count, followers = accounts.get_followers_and_following()
+        posts = accounts.post.all()
+        return render(request, 'account.html', {'accounts': accounts, 'following_count': following_count, 'following': following, 'followers_count': followers_count, 'followers': followers, 'posts': posts})
+
+class SearchAccountView(View):
+    def get(self, request):
+        query = request.GET.get('q')
+        accounts = Account.objects.filter(Q(username__icontains=query))
+        context = {'accounts': accounts}
+        return render(request, 'search_account.html', context)
