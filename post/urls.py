ebc8566e6e668834750bd188e1c5f3ce9e3a1ad1
hesel <hgharehbaai@gmail.com>
2023-06-27 09:07:15 +0330

    delete twitter_social_network and its apps

--- post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('post/<str:post_title>/', views.post, name= 'post'),
-    path('search/post', views.search_posts, name='search posts'),
-]
